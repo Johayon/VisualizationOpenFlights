5be9eaf5e528d27347af39c09cfb5e125addf184
AdrienDutertre <adriendutertre89@gmail.com>
2015-01-30 09:43:49 +0100

    Ajout du barChart en version alpha

---
+++ barChart/barChart.js
@@ -0,0 +1,183 @@
+// Les variables globales 
+var csvDataFilePath = "../data/dataForBarChartVisualization.csv"
+var alldata = new Array();
+var data = [];
+var chart = null;
+var minimumNbRoutes = 0;
+var maximumNbRoutes = 10000;
+var isLongHaulSelected = true;
+var isMediumHaulSelected = true;
+var isShortHaulSelected = true;
+
+
+function loadDataAndDrawGraph()
+{
+    // Load the data
+    d3.csv(csvDataFilePath, function(companies)
+    {   
+        // Structure
+        var tab1 = {key:'Vols_Court_Courrier', values:[]};
+        var tab2 = {key:'Vols_Moyen_Courrier', values:[]};
+        var tab3 = {key:'Vols_Longs_Courrier', values:[]};
+        data = [tab1, tab2, tab3]
+
+        var numberOfCompanies = companies.length;
+        for (var i = 0; i < numberOfCompanies; i++) 
+        {
+            // Récupération des données brutes 
+            var name = companies[i].Airline;
+            var shortHaul = parseInt(companies[i]['0']);
+            var mediumHaul = parseInt(companies[i]['1']);
+            var longHaul = parseInt(companies[i]['2']);
+
+            // Ajout à la structure 
+            data[0].values.push( {x:name, y:shortHaul} )
+            data[1].values.push( {x:name, y:mediumHaul} )
+            data[2].values.push( {x:name, y:longHaul} )
+
+            alldata[name] = {shortHaul: shortHaul, mediumHaul: mediumHaul, longHaul: longHaul}
+        }
+
+        console.log("Data is now loaded : ", data)
+        drawGraph()
+    })   
+}
+
+
+// Fonction dessinant le graphique
+function drawGraph()
+{
+    nv.addGraph(function()
+    {
+		chart = nv.models.multiBarChart();
+        chart.reduceXTicks(false);
+        chart.multibar.stacked(true);
+		chart.yAxis.tickFormat(d3.format('d'));        
+		d3.select('#chart svg').datum(data).transition().duration(500).call(chart);
+		nv.utils.windowResize(chart.update);
+	},  function()
+        {
+            // Premièrement on ne selectionne que les 3 boutons short, medium et long haul
+            d3.selectAll(".nv-series")[0][0].onclick = selectShortFlight;
+            d3.selectAll(".nv-series")[0][1].onclick = selectMediumFlight;
+            d3.selectAll(".nv-series")[0][2].onclick = selectLongFlight;
+            sortGraphData();
+        }
+    )    
+}
+
+
+// Fonctions prenant en compte le clic sur un type de vol
+function selectShortFlight()
+{
+    console.log("In short fonction")
+    isShortHaulSelected = !isShortHaulSelected;
+    checkIfAllFlightsTypeAreDisabled();
+    sortGraphData();
+}
+
+
+// Fonctions prenant en compte le clic sur un type de vol
+function selectMediumFlight()
+{
+    console.log("In medium fonction")
+    isMediumHaulSelected = !isMediumHaulSelected;
+    checkIfAllFlightsTypeAreDisabled();
+    sortGraphData();
+}
+
+
+// Fonctions prenant en compte le clic sur un type de vol
+function selectLongFlight()
+{
+    console.log("In long fonction")
+    isLongHaulSelected = !isLongHaulSelected;
+    checkIfAllFlightsTypeAreDisabled();
+    sortGraphData();
+}
+
+
+// Si tous les types de vols sont desactivés alors nvd3 les réactive tous...
+function checkIfAllFlightsTypeAreDisabled()
+{
+    if( !isShortHaulSelected && !isMediumHaulSelected && !isLongHaulSelected )
+    {
+        isShortHaulSelected = true;
+        isMediumHaulSelected = true;
+        isLongHaulSelected = true;
+    }
+}
+
+
+// Fonction qui trie les données du graphe 
+function sortGraphData() 
+{
+    data[0].values.sort(sortArray);
+    data[1].values.sort(sortArray);
+    data[2].values.sort(sortArray);
+    d3.select('#chart svg').datum(data).transition().duration(500).call(chart);
+}
+
+
+function sortArray(a, b)
+{
+    var aName = a.x;
+    var bName = b.x;
+    var aTotal = 0;
+    var bTotal = 0;
+
+    if( isShortHaulSelected )
+    {
+        aTotal += alldata[aName].shortHaul;
+        bTotal += alldata[bName].shortHaul;
+    }
+
+    if( isMediumHaulSelected )
+    {
+        aTotal += alldata[aName].mediumHaul;
+        bTotal += alldata[bName].mediumHaul;
+    }
+
+    if( isLongHaulSelected )
+    {
+        aTotal += alldata[aName].longHaul;
+        bTotal += alldata[bName].longHaul;
+    }
+
+    return (aTotal < bTotal)
+}
+
+
+function filterGraphDataByTotalOfRoutes(minimum, maximum)
+{
+    // Mise à jour des variables globales
+    minimumNbRoutes = minimum;
+    maximumNbRoutes = maximum;
+
+    // Formatage des données pour coller au format du graphe nvd3
+    data[0].values = [];
+    data[1].values = [];
+    data[2].values = [];
+
+    console.log("Test stylé", alldata)
+
+    for(var el in alldata)
+    {
+        var totalNbRoutes = alldata[el].shortHaul + alldata[el].mediumHaul + alldata[el].longHaul;
+        if( totalNbRoutes > minimum && totalNbRoutes < maximum )
+        {
+            data[0].values.push( {x:el, y:alldata[el].shortHaul} )
+            data[1].values.push( {x:el, y:alldata[el].mediumHaul} )
+            data[2].values.push( {x:el, y:alldata[el].longHaul} )
+        }
+    }
+
+    // Tri des nouvelles données 
+    sortGraphData();
+    
+    // Mise à jour du graphe
+    d3.select('#chart svg').datum(data).transition().duration(500).call(chart);
+}
+
+
+loadDataAndDrawGraph();
---
+++ barChart/createCsvForBarChartVisu.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pd 
+import geopy
+import geopy.distance
+
+
+def readAndMergeData(airlinesDataFile, airportsDataFile, routesDataFile) : 
+
+	# Première étape : on lit les 3 csv et on les stocke chacun dans un DF 
+	airportsDF = pd.read_csv(airportsDataFile, na_values=['\N'])
+	routesDF = pd.read_csv(routesDataFile, na_values=['\N'])
+	airlinesDF = pd.read_csv(airlinesDataFile, na_values=['\N'])
+	
+	# On ne garde que ce qui nous interesse
+	routesDF.drop([u'Codeshare'], axis=1, inplace=True)
+	airportsDF.drop([u'Name', u'City', u'Country', u'IATA/FAA', u'ICAO', u'Altitude', u'Timezone', u'DST', u'Tz database time zone'], axis=1, inplace=True)
+	airlinesDF.drop([u'Alias', u'IATA', u'ICAO', u'Callsign', u'Country', u'Active'], axis=1, inplace=True)
+	
+	# On supprime les valeurs nan
+	routesDF.dropna(inplace=True)
+	airportsDF.dropna(inplace=True)
+	airlinesDF.dropna(inplace=True)
+
+	# Jointure pour les sources airports 
+	airportsDF.columns = [u'Source airport ID', u'Source latitude', u'Source longitude']
+	tmpDF = pd.merge(routesDF, airportsDF, on='Source airport ID')
+
+	# Jointure pour les destination airports 
+	airportsDF.columns = [u'Destination airport ID', u'Destination latitude', u'Destination longitude']
+	tmp2DF = pd.merge(tmpDF, airportsDF, on='Destination airport ID')
+
+	# Jointure pour les noms complets des airlines 
+	mergedDF = pd.merge(tmp2DF, airlinesDF, on='Airline ID')
+
+	# Enfin on retourne l'unique DF résultant où figurent toutes les infos dont on a besoin ensuite 
+	return mergedDF
+
+
+def computeFlightDistance(row) : 
+
+	sourcePoint = geopy.Point(row['Source latitude'], row['Source longitude'])
+	destinationPoint = geopy.Point(row['Destination latitude'], row['Destination longitude'])
+
+	return geopy.distance.distance(sourcePoint, destinationPoint).km
+
+
+def getFlightType(row, thresholds) : 
+
+	# court courrier  
+	if row['distance'] < thresholds[0] : 
+		return 0
+
+	# Moyen courrier
+	elif row['distance'] < thresholds[1] :
+		return 1
+
+	# Long courrier
+	else : 
+		return 2
+
+
+# Paramètres du programme
+airlinesDataFile = "../data/airlines.csv"
+airportsDataFile = "../data/airports.csv"
+routesDataFile = "../data/routes.csv"
+csvOutputFilename = "../data/dataForBarChartVisualization.csv"
+thresholds = [1500, 3000]
+
+# Récupération des données brutes
+print "\nRécupération des données brutes"
+data = readAndMergeData(airlinesDataFile, airportsDataFile, routesDataFile)
+
+# Pour chaque vol calcul de la distance parcourue et donc ensuite du type de vol
+print "Calcul de la distance et du type de chaque vol"
+data['distance'] = data.apply(computeFlightDistance, axis=1)
+data['Type'] = data.apply(lambda x : getFlightType(x, thresholds), axis=1)
+
+# Regroupement du nombre de vols de chaque type par compagnie aérienne
+print "Regroupement du nombre de vols de chaque type par compagnie"
+dataForVisualization = data.groupby(['Airline', 'Name'])['Type'].value_counts().unstack()
+dataForVisualization.fillna(0, inplace=True)
+
+# Ecriture du csv qui servira pour la partie visu en Javascript ensuite
+print "Ecriture du csv"
+dataForVisualization.to_csv(csvOutputFilename)
