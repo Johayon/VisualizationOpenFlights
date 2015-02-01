57712b2606a9b6a5b302ed5eb04e79087b779e7e
yjanvier <yoann.janvier@telecom-paristech.fr>
2015-02-01 22:15:42 +0100

    Avancées sur bar charts

--- barChart/barChart.js
+++ barChart/barChart.js
@@ -1,13 +1,44 @@
 // Les variables globales 
 var csvDataFilePath = "dataForBarChartVisualization.csv"
+var csvAirportsListPerAirlineFilePath =  "dataAirportsListPerAirline.csv"
 var alldata = new Array();
 var data = [];
 var chart = null;
 var minimumNbRoutes = 0;
 var maximumNbRoutes = 10000;
+///// A DISCUTER AVEC ADRIEN => LIMITE D'AFFICHAGE DU NOMBRE DE COMPANGNIES + QUEL TRI POUR L'AFFICHAGE/////
+var overallNbRoutes= 67663; // assumption : same route operated by 2 airlines = 2 routes
+var overallNbAirports= 8107; 
+var totalNbRoutesDisplayed = 0; // To calculate the ratio of number of routes displayed
+var overallNbCountries=240;
 var isLongHaulSelected = true;
 var isMediumHaulSelected = true;
 var isShortHaulSelected = true;
+var airportsWholeSet = new Set();
+var gaugeAirportsValue = 0;
+var gaugeRoutesValue = 0;
+gaugeCountriesValue = 0;
+var gaugeAirports = new JustGage({
+                    id: "gaugeAirports",
+                    value: gaugeAirportsValue,
+                    min: 0,
+                    max: overallNbAirports,
+            title: "Airports Coverage"
+         });
+var gaugeRoutes = new JustGage({
+                    id: "gaugeRoutes",
+                    value: gaugeRoutesValue,
+                    min: 0,
+                    max: overallNbRoutes,
+            title: "Routes"
+         });
+var gaugeCountries = new JustGage({
+                    id: "gaugeCountries",
+                    value: gaugeCountriesValue,
+                    min: 0,
+                    max: overallNbCountries,
+            title: "Countries reached"
+         });
 
 
 function loadDataAndDrawGraph()
@@ -29,18 +60,42 @@ function loadDataAndDrawGraph()
             var shortHaul = parseInt(companies[i]['0']);
             var mediumHaul = parseInt(companies[i]['1']);
             var longHaul = parseInt(companies[i]['2']);
+            var fullName = companies[i].Name
+            var country = companies[i].Country
+            var numberCountries = companies[i]['Number Countries']
+            var favoriteAirportName = companies[i]['Favorite Airport Name']
+            var favoriteAirportCity = companies[i]['Favorite Airport City']
+            var favoriteAirportCountry = companies[i]['Favorite Airport Country']
+            var airportsSet = new Set();
 
             // Ajout à la structure 
             data[0].values.push( {x:name, y:shortHaul} )
             data[1].values.push( {x:name, y:mediumHaul} )
             data[2].values.push( {x:name, y:longHaul} )
 
-            alldata[name] = {shortHaul: shortHaul, mediumHaul: mediumHaul, longHaul: longHaul}
+            alldata[name] = {shortHaul: shortHaul, mediumHaul: mediumHaul, longHaul: longHaul, fullName:fullName, country:country, numberCountries :numberCountries, 
+                            favoriteAirportName :favoriteAirportName, favoriteAirportCity : favoriteAirportCity, favoriteAirportCountry : favoriteAirportCountry, airportsSet : airportsSet}
         }
 
         console.log("Data is now loaded : ", data)
         drawGraph()
-    })   
+    })
+     
+
+    d3.csv(csvAirportsListPerAirlineFilePath, function(companies)
+    {
+        var numberOfCompanies = companies.length;
+        for (var i = 0; i < numberOfCompanies; i++) 
+        {
+            // Récupération des données brutes 
+            var name = companies[i].Airline;
+            var airportID = companies[i]['Airport ID']
+            if (name in alldata) {
+            alldata[name].airportsSet.add(airportID); 
+            }         
+        }
+    drawGraph()
+    })
 }
 
 
@@ -61,13 +116,61 @@ function drawGraph()
             d3.selectAll(".nv-series")[0][0].onclick = selectShortFlight;
             d3.selectAll(".nv-series")[0][1].onclick = selectMediumFlight;
             d3.selectAll(".nv-series")[0][2].onclick = selectLongFlight;
+            
+            //Added for managing clicks on bars 
+            chart.multibar.dispatch.on("elementClick", function(e) {
+            var position = e.pointIndex;
+            console.log(e);
+            refreshAirlinePanel(e.series.values[position].x);
+            }
+            )
+
             sortGraphData();
             filterGraphDataByTotalOfRoutes(500, 1500);
+        
         }
-    )    
+            )    
 }
 
 
+function displayAirlinePanel(a)
+{
+    
+}
+
+function refreshGeneralPanel(valueAirports,totalNbRoutesDisplayed)
+{
+    gaugeAirports.refresh(valueAirports);
+    gaugeAirportsValue=valueAirports;
+    gaugeRoutes.refresh(totalNbRoutesDisplayed);
+    gaugeRoutesValue=totalNbRoutesDisplayed;
+}
+
+function refreshAirlinePanel(a)
+{
+    gaugeCountries.refresh(alldata[a].numberCountries);
+    gaugeCountriesValue=alldata[a].numberCountries;
+    d3.select("#airlineName").text("AirLine Name: " + alldata[a].fullName);
+    d3.select("#country").text("Country of Origin: " + alldata[a].country);
+    // d3.select("#panel").select("#numberCountries").text("Number of countries covered by airline: " + alldata[a].numberCountries);
+    d3.select("#mainAirport").text("Main airport: " + alldata[a].favoriteAirportName + ", " + alldata[a].favoriteAirportCity + ", " + alldata[a].favoriteAirportCountry);
+}
+
+// function displayGeneralPanel(value, min, max)
+// {
+//     var percentageRoutesDisplayed =  ((totalNbRoutesDisplayed / overallNbRoutes)*100).toFixed(2);
+//     var airportsSetNumber = airportsWholeSet.size;
+//     var percentageAirportsReached = ((airportsWholeSet.size/overallNbAirports)*100).toFixed(2); 
+//     // d3.select("#panel").select("#percentageRoutes").text("Percentage of routes covered by the selection: " + percentageRoutesDisplayed.toString() + " %");
+//     // d3.select("#panel").select("#percentageAirports").text("Percentage of airports reached by the selection: " + percentageAirportsReached.toString() + " %");
+//     var g = new JustGage({
+//             id: "gaugeAirports",
+//             value: 67,
+//             min: 0,
+//             max: 100,
+//             title: "Airports"
+//             });
+// }
 // Fonctions prenant en compte le clic sur un type de vol
 function selectShortFlight()
 {
@@ -154,6 +257,8 @@ function filterGraphDataByTotalOfRoutes(minimum, maximum)
     // Mise à jour des variables globales
     minimumNbRoutes = minimum;
     maximumNbRoutes = maximum;
+    totalNbRoutesDisplayed = 0;
+    airportsWholeSet = new Set();
 
     // Formatage des données pour coller au format du graphe nvd3
     data[0].values = [];
@@ -167,9 +272,15 @@ function filterGraphDataByTotalOfRoutes(minimum, maximum)
         var totalNbRoutes = alldata[el].shortHaul + alldata[el].mediumHaul + alldata[el].longHaul;
         if( totalNbRoutes > minimum && totalNbRoutes < maximum )
         {
-            data[0].values.push( {x:el, y:alldata[el].shortHaul} )
-            data[1].values.push( {x:el, y:alldata[el].mediumHaul} )
-            data[2].values.push( {x:el, y:alldata[el].longHaul} )
+            data[0].values.push( {x:el, y:alldata[el].shortHaul} );
+            data[1].values.push( {x:el, y:alldata[el].mediumHaul} );
+            data[2].values.push( {x:el, y:alldata[el].longHaul} );
+            totalNbRoutesDisplayed += totalNbRoutes;
+
+            for (id of alldata[el].airportsSet) 
+            {
+                airportsWholeSet.add(id)
+            }
         }
     }
 
@@ -178,7 +289,41 @@ function filterGraphDataByTotalOfRoutes(minimum, maximum)
     
     // Mise à jour du graphe
     d3.select('#chart svg').datum(data).transition().duration(500).call(chart);
+    
+    refreshGeneralPanel(airportsWholeSet.size, totalNbRoutesDisplayed);
 }
-
-
 loadDataAndDrawGraph();
+
+
+///CODE TO REFRESH THE GAUGE
+// var gageValue = 100;
+
+//         var g = new JustGage({
+//             id: "gauge",
+//             value: gageValue,
+//             min: 0,
+//             max: 200,
+//             title: "BMI"
+//         });
+
+// function updateGage(n) {
+//   g.refresh(gageValue + n);
+//   gageValue += n;
+// }
+
+// function inc() {
+//   updateGage(20);
+// }
+
+// function dec() {
+//   updateGage(-20);
+// }
+
+// function incVar() {
+//   updateGage(parseInt(document.getElementById("change").value))
+// }
+
+// function decVar() {
+//   updateGage(-parseInt(document.getElementById("change").value))
+// }
+
--- barChart/createCsvForBarChartVisu.py
+++ barChart/createCsvForBarChartVisu.py
@@ -15,8 +15,9 @@ def readAndMergeData(airlinesDataFile, airportsDataFile, routesDataFile) :
 	
 	# On ne garde que ce qui nous interesse
 	routesDF.drop([u'Codeshare'], axis=1, inplace=True)
-	airportsDF.drop([u'Name', u'City', u'Country', u'IATA/FAA', u'ICAO', u'Altitude', u'Timezone', u'DST', u'Tz database time zone'], axis=1, inplace=True)
-	airlinesDF.drop([u'Alias', u'IATA', u'ICAO', u'Callsign', u'Country', u'Active'], axis=1, inplace=True)
+	airportsDF.drop([u'IATA/FAA', u'ICAO', u'Altitude', u'Timezone', u'DST', u'Tz database time zone', u'Name', u'City', u'Country'], axis=1, inplace=True)
+
+	airlinesDF.drop([u'Alias', u'IATA', u'ICAO', u'Callsign', u'Active'], axis=1, inplace=True)
 	
 	# On supprime les valeurs nan
 	routesDF.dropna(inplace=True)
@@ -60,17 +61,92 @@ def getFlightType(row, thresholds) :
 	else : 
 		return 2
 
+def getNumberCountriesCovered(airportsDataFile, routesDataFile) :
+	# Première étape : on lit les 3 csv et on les stocke chacun dans un DF 
+	airportsDF = pd.read_csv(airportsDataFile, na_values=['\N'])
+	routesDF = pd.read_csv(routesDataFile, na_values=['\N'])
+
+	# On ne garde que ce qui nous interesse
+	routesDF.drop([u'Codeshare',u'Stops', u'Equipment', u'Airline ID', u'Source airport', u'Destination airport'], axis=1, inplace=True)
+	airportsDF.drop([u'IATA/FAA', u'ICAO', u'Altitude', u'Timezone', u'DST', u'Tz database time zone',u'Latitude', u'Longitude'], axis=1, inplace=True)
+
+	# On supprime les valeurs nan
+	routesDF.dropna(inplace=True)
+	airportsDF.dropna(inplace=True)
+
+	# Jointure pour les sources airports 
+	resultMerge = pd.merge (routesDF, airportsDF, left_on='Source airport ID', right_on='Airport ID')
+
+	# Jointure pour les destination airports 
+	resultMerge2 = pd.merge (resultMerge, airportsDF, left_on='Destination airport ID', right_on='Airport ID')
+	resultMerge2 = resultMerge2[['Airline', 'Country_x', 'Country_y']]
+
+	#Concaténation et groupby
+	resultMerge2=pd.concat([resultMerge2[['Airline', 'Country_x']].rename(columns={'Country_x':'Number Countries'}),resultMerge2[['Airline','Country_y']].rename(columns={'Country_y':'Number Countries'})]).drop_duplicates()
+	nbCountriesCoveredDF = resultMerge2.groupby('Airline').count()
+	return nbCountriesCoveredDF
+
+def getFavoriteAirport(airportsDataFile, routesDataFile) :
+	# Première étape : on lit les 3 csv et on les stocke chacun dans un DF 
+	airportsDF = pd.read_csv(airportsDataFile, na_values=['\N'])
+	routesDF = pd.read_csv(routesDataFile, na_values=['\N'])
+
+	# On ne garde que ce qui nous interesse
+	routesDF.drop([u'Codeshare',u'Stops', u'Equipment', u'Airline ID', u'Source airport', u'Destination airport'], axis=1, inplace=True)
+	airportsDF.drop([u'IATA/FAA', u'ICAO', u'Altitude', u'Timezone', u'DST', u'Tz database time zone',u'Latitude', u'Longitude'], axis=1, inplace=True)
+
+	# On supprime les valeurs nan
+	routesDF.dropna(inplace=True)
+	airportsDF.dropna(inplace=True)
+
+	# On crée des DataFrames pour les lieux de départ et de destination de chaque route
+	sourceRouteDF = routesDF.drop([u'Destination airport ID'], axis=1).rename(columns={'Source airport ID':'Airport ID'})
+	destinationRouteDF = routesDF.drop([u'Source airport ID'], axis=1).rename(columns={'Destination airport ID':'Airport ID'})
+
+	# On compte le nombre d'occurrence de chaque lieu pour chaque Airline
+	result = pd.concat ([sourceRouteDF, destinationRouteDF])
+	result['Value']=1
+	result = result.groupby(['Airline', 'Airport ID']).count().reset_index()
+	favoriteAirportDF = result.groupby('Airline').apply(lambda subf: subf['Airport ID'][subf['Value'].idxmax()]).to_frame('Airport ID')
+	favoriteAirportDF = favoriteAirportDF.reset_index().merge(airportsDF, on='Airport ID').set_index('Airline')
+	favoriteAirportDF = favoriteAirportDF.rename(columns = {'Airport ID' : 'Favorite Airport ID', 'Name' : 'Favorite Airport Name' , 'City' : 'Favorite Airport City', 'Country' : 'Favorite Airport Country'})
+	return favoriteAirportDF
+
+def getAirportsList(airportsDataFile, routesDataFile) :
+	# Première étape : on lit les 3 csv et on les stocke chacun dans un DF 
+	airportsDF = pd.read_csv(airportsDataFile, na_values=['\N'])
+	routesDF = pd.read_csv(routesDataFile, na_values=['\N'])
+
+	# On ne garde que ce qui nous interesse
+	routesDF.drop([u'Codeshare',u'Stops', u'Equipment', u'Airline ID', u'Source airport', u'Destination airport'], axis=1, inplace=True)
+	airportsDF.drop([u'IATA/FAA', u'ICAO', u'Altitude', u'Timezone', u'DST', u'Tz database time zone',u'Latitude', u'Longitude'], axis=1, inplace=True)
+
+	# On supprime les valeurs nan
+	routesDF.dropna(inplace=True)
+	airportsDF.dropna(inplace=True)
+
+	# On crée des DataFrames pour les lieux de départ et de destination de chaque route
+	sourceRouteDF = routesDF.drop([u'Destination airport ID'], axis=1).rename(columns={'Source airport ID':'Airport ID'})
+	destinationRouteDF = routesDF.drop([u'Source airport ID'], axis=1).rename(columns={'Destination airport ID':'Airport ID'})
+
+	result = pd.concat ([sourceRouteDF, destinationRouteDF]).set_index('Airline')
+	return result
+	
 
 # Paramètres du programme
 airlinesDataFile = "../data/airlines.csv"
 airportsDataFile = "../data/airports.csv"
 routesDataFile = "../data/routes.csv"
 csvOutputFilename = "dataForBarChartVisualization.csv"
+csvAirportListOutputFilename = "dataAirportsListPerAirline.csv"
 thresholds = [1500, 3000]
 
 # Récupération des données brutes
 print "\nRécupération des données brutes"
 data = readAndMergeData(airlinesDataFile, airportsDataFile, routesDataFile)
+nbCountriesCoveredDF = getNumberCountriesCovered(airportsDataFile, routesDataFile)
+favoritesAirportsDF = getFavoriteAirport(airportsDataFile, routesDataFile)
+airportList = getAirportsList(airportsDataFile, routesDataFile)
 
 # Pour chaque vol calcul de la distance parcourue et donc ensuite du type de vol
 print "Calcul de la distance et du type de chaque vol"
@@ -79,9 +155,34 @@ data['Type'] = data.apply(lambda x : getFlightType(x, thresholds), axis=1)
 
 # Regroupement du nombre de vols de chaque type par compagnie aérienne
 print "Regroupement du nombre de vols de chaque type par compagnie"
-dataForVisualization = data.groupby(['Airline', 'Name'])['Type'].value_counts().unstack()
+dataForVisualization = data.groupby(['Airline', 'Name', 'Country'])['Type'].value_counts().unstack()
 dataForVisualization.fillna(0, inplace=True)
 
+# Regroupement du nombre de pays couverts par chaque compagnie aérienne
+dataForVisualization = dataForVisualization.join(nbCountriesCoveredDF)
+dataForVisualization = dataForVisualization.join(favoritesAirportsDF)
+
 # Ecriture du csv qui servira pour la partie visu en Javascript ensuite
-print "Ecriture du csv"
+print "Ecriture des csv"
 dataForVisualization.to_csv(csvOutputFilename)
+airportList.to_csv(csvAirportListOutputFilename)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
